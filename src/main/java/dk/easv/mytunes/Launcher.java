7601a89d52e710b850ffb1aeb30afcbe297521a7
DESKTOP-V4I32ES\Dada <david.terek2005@gmail.com>
2025-11-25 11:29:13 +0100

    push the new version where music can be played and stopped

--- src/main/java/dk/easv/mytunes/Launcher.java
+++ src/main/java/dk/easv/mytunes/Launcher.java
@@ -1,6 +1,9 @@
 package dk.easv.mytunes;
 
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 public class Launcher {
     public static void main(String[] args) {
